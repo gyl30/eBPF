0bb34097952e91bc8b196bcaac452dec3996c89c
gyl30 <im.ggyyll@gmail.com>
2024-10-28 15:51:19 +0800

    修改转发路径

--- xdp/xdp_redirect.c
+++ xdp/xdp_redirect.c
@@ -7,18 +7,19 @@
 #include <bpf/bpf_endian.h>
 
 // 定义常量
-#define SRC_IP_ENO1 0xAC142C51    // 172.20.44.81
-#define SRC_PORT_C 3333
-#define SRC_PORT_D 6666
+#define SRC_IP 0xAC143E63    // // 172.20.62.99
+#define SRC_PORT 3333
 
-#define DST_IP_ENO1 0xAC142C65    // 172.20.44.101
-#define DST_PORT_A 1188
-#define DST_PORT_B 8811
+#define PROXY_IP 0xAC142C51    // 172.20.44.81
+#define PROXY_PORT 6666
+
+#define DST_IP 0xAC142C65    // 172.20.44.101
+#define DST_PORT 8811
 
 #define ENO1_IFINDEX 2    // 172.20.44.81 的网卡名名称是 eno1，索引是 2
 
-// 从 172.20.44.81:3333 到 172.20.44.81:1188 的数据包修改为 从 172.20.44.81:6666 到 172.20.44.101:8811
-// 从 172.20.44.101:8811 到 172.20.44.81:6666 的数据包修改为 从 172.20.44.81:1188 到 172.20.44.81:3333
+// 从 172.20.62.99:3333 到 172.20.44.81:6666 的数据包修改为 从 172.20.44.81:6666 到 172.20.44.101:8811
+// 从 172.20.44.101:8811 到 172.20.44.81:6666 的数据包修改为 从 172.20.44.81:6666 到 172.20.62.99:3333
 
 static __always_inline void update_mac(struct ethhdr *eth, __u8 *src_mac, __u8 *dst_mac)
 {
@@ -68,51 +69,55 @@ int xdp_redirect(struct xdp_md *ctx)
     {
         return XDP_PASS;
     }
+    __u8 src_mac[ETH_ALEN] = {0x00, 0x0c, 0x29, 0x16, 0x61, 0x2d};
+    __u8 proxy_mac[ETH_ALEN] = {0x58, 0x11, 0x22, 0xc3, 0x23, 0xe6};
+    __u8 dst_mac[ETH_ALEN] = {0xd0, 0x94, 0x66, 0xf1, 0x96, 0xae};
 
-    bpf_printk("Received packet: src port=%d, dst port=%d\n", bpf_ntohs(tcp->source), bpf_ntohs(tcp->dest));
+    // bpf_printk("Received packet: src port=%d, dst port=%d\n", bpf_ntohs(tcp->source), bpf_ntohs(tcp->dest));
 
-    // 从 172.20.44.81:3333 到 172.20.44.81:1188 的数据包修改为 从 172.20.44.81:6666 到 172.20.44.101:8811
-    if (ip->saddr == bpf_htonl(SRC_IP_ENO1) && tcp->source == bpf_htons(SRC_PORT_C) && ip->daddr == bpf_htonl(SRC_IP_ENO1) && tcp->dest == bpf_htons(DST_PORT_A))
+    // 从 172.20.62.99:3333 到 172.20.44.81:6666 的数据包修改为 从 172.20.44.81:6666 到 172.20.44.101:8811
+    if (ip->saddr == bpf_htonl(SRC_IP) && tcp->source == bpf_htons(SRC_PORT) && ip->daddr == bpf_htonl(PROXY_IP) && tcp->dest == bpf_htons(PROXY_PORT))
     {
-        // 修改 MAC 地址
-        __u8 eno1_mac[ETH_ALEN] = {0x58, 0x11, 0x22, 0xc3, 0x23, 0xe6};
-        __u8 other_mac[ETH_ALEN] = {0xd0, 0x94, 0x66, 0xf1, 0x96, 0xae};
-        update_mac(eth, eno1_mac, other_mac);
-        // 172.20.44.81:3333 到 172.20.44.81:1188 修改为 172.20.44.81:6666 到 172.20.44.101:8811
-        ip->saddr = bpf_htonl(SRC_IP_ENO1);     // 172.20.44.81
-        ip->daddr = bpf_htonl(DST_IP_ENO1);     // 172.20.44.101
-        tcp->source = bpf_htons(SRC_PORT_D);    // 6666
-        tcp->dest = bpf_htons(DST_PORT_B);      // 8811
+        // update mac
+        update_mac(eth, proxy_mac, dst_mac);
+        // update ip
+        ip->saddr = bpf_htonl(PROXY_IP);
+        ip->daddr = bpf_htonl(DST_IP);
+        // update tcp
+        tcp->source = bpf_htons(PROXY_PORT);
+        tcp->dest = bpf_htons(DST_PORT);
+        // checksum
         tcp->check = 0;
         ip->check = 0;
-        tcp->check = bpf_csum_diff(0, 0, (void *)tcp, sizeof(struct tcphdr), tcp->check);
-        ip->check = bpf_csum_diff(0, 0, (void *)ip, sizeof(struct iphdr), ip->check);
-        ret = bpf_redirect(ENO1_IFINDEX, 0);
-        bpf_printk("Redirecting eno1 to other: src port=%d, dst port=%d redirect ret=%d\n", bpf_ntohs(tcp->source), bpf_ntohs(tcp->dest), ret);
+        tcp->check = tcp_checksum(tcp, sizeof(struct tcphdr));
+        ip->check = ip_checksum(ip, sizeof(struct iphdr));
+        // ret = bpf_redirect(ENO1_IFINDEX, 0);
+        // ret = XDP_TX;
+        ret = XDP_PASS;
+        bpf_printk("Redirecting from src to dst: src port=%d, dst port=%d redirect ret=%d\n", bpf_ntohs(tcp->source), bpf_ntohs(tcp->dest), ret);
         return ret;
-        // return XDP_PASS;
     }
-    // 从 172.20.44.101:8811 到 172.20.44.81:6666 的数据包修改为 从 172.20.44.81:1188 到 172.20.44.81:3333
-    else if (ip->saddr == bpf_htonl(DST_IP_ENO1) && tcp->source == bpf_htons(DST_PORT_B) && ip->daddr == bpf_htonl(SRC_IP_ENO1) && tcp->dest == bpf_htons(SRC_PORT_D))
+    // 从 172.20.44.101:8811 到 172.20.44.81:6666 的数据包修改为 从 172.20.44.81:6666 到 172.20.62.99:3333
+    else if (ip->saddr == bpf_htonl(DST_IP) && tcp->source == bpf_htons(DST_PORT) && ip->daddr == bpf_htonl(PROXY_IP) && tcp->dest == bpf_htons(PROXY_PORT))
     {
-        // 修改 MAC 地址
-        __u8 eno1_mac[ETH_ALEN] = {0x58, 0x11, 0x22, 0xc3, 0x23, 0xe6};
-        __u8 other_mac[ETH_ALEN] = {0xd0, 0x94, 0x66, 0xf1, 0x96, 0xae};
-
-        update_mac(eth, eno1_mac, eno1_mac);
-
-        ip->saddr = bpf_htonl(SRC_IP_ENO1);     // 172.20.44.81
-        ip->daddr = bpf_htonl(SRC_IP_ENO1);     // 172.20.44.81
-        tcp->source = bpf_htons(DST_PORT_A);    // 1188
-        tcp->dest = bpf_htons(SRC_PORT_C);      // 3333
+        // update mac
+        update_mac(eth, proxy_mac, src_mac);
+        // update ip
+        ip->saddr = bpf_htonl(PROXY_IP);
+        ip->daddr = bpf_htonl(SRC_IP);
+        // update tcp
+        tcp->source = bpf_htons(PROXY_PORT);
+        tcp->dest = bpf_htons(SRC_PORT);
+        // checksum
         tcp->check = 0;
         ip->check = 0;
-        tcp->check = bpf_csum_diff(0, 0, (void *)tcp, sizeof(struct tcphdr), tcp->check);
-        ip->check = bpf_csum_diff(0, 0, (void *)ip, sizeof(struct iphdr), ip->check);
-        ret = bpf_redirect(ENO1_IFINDEX, 0);
-        bpf_printk("Redirecting other to eno1: src port=%d, dst port=%d redirect ret=%d\n", bpf_ntohs(tcp->source), bpf_ntohs(tcp->dest), ret);
+        tcp->check = tcp_checksum(tcp, sizeof(struct tcphdr));
+        ip->check = ip_checksum(ip, sizeof(struct iphdr));
+        // ret = bpf_redirect(ENO1_IFINDEX, 0);
+        // ret = XDP_TX;
+        ret = XDP_PASS;
+        bpf_printk("Redirecting from dst to src: src port=%d, dst port=%d redirect ret=%d\n", bpf_ntohs(tcp->source), bpf_ntohs(tcp->dest), ret);
         return ret;
-        // return XDP_PASS;
     }
 
     return XDP_PASS;
