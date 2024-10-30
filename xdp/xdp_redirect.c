aae2366d9dfea86ced8e123b9f80ec8b0571017c
gyl30 <im.ggyyll@gmail.com>
2024-10-30 21:25:15 +0800

    重定向时不检查源端口

--- xdp/xdp_redirect.c
+++ xdp/xdp_redirect.c
@@ -9,29 +9,28 @@
 #include <bpf/bpf_core_read.h>
 
 // 定义常量
-#define SRC_IP 0xAC143E63    // 172.20.62.99
-#define SRC_PORT 3333
-
+#define SRC_IP 0xAC143E63      // 172.20.62.99
 #define PROXY_IP 0xAC142C51    // 172.20.44.81
-#define PROXY_PORT 6666 
-
-#define DST_IP 0xAC142C65    // 172.20.44.101
-#define DST_PORT 8811
-
-#define ENO1_IFINDEX 2    // eno1 网卡的索引
-
-// 更新 MAC 地址
+#define DST_IP 0xAC142C65      // 172.20.44.101
+#define TARGET_PORT 8811       // 目标端口，遇到此端口的数据包才进行处理
+#define DEVICE_IFINDEX 2       // 转发网卡的索引
+//
 static __always_inline void update_mac(struct ethhdr *eth, __u8 *src_mac, __u8 *dst_mac)
 {
     __builtin_memcpy(eth->h_source, src_mac, ETH_ALEN);
     __builtin_memcpy(eth->h_dest, dst_mac, ETH_ALEN);
 }
-static __always_inline __u16 csum_fold_helper(__u32 csum)
+
+static __u16 csum_fold_helper(__u64 csum)
 {
-    __u32 r = csum << 16 | csum >> 16;
-    csum = ~csum;
-    csum -= r;
-    return (__u16)(csum >> 16);
+    int i;
+#pragma unroll
+    for (i = 0; i < 4; i++)
+    {
+        if (csum >> 16)
+            csum = (csum & 0xffff) + (csum >> 16);
+    }
+    return ~csum;
 }
 static __always_inline void iph_csum(struct iphdr *iph)
 {
@@ -92,7 +91,7 @@ static __always_inline void update_headers(void *data,
     tcp->dest = new_dst_port;
     tcp->check = csum_diff4(oldsrcport, tcp->source, tcp->check);
     tcp->check = csum_diff4(olddstport, tcp->dest, tcp->check);
-    bpf_printk("TCP checksum updated from %x to %x\n", old_tcp_csum, tcp->check);
+    // bpf_printk("TCP checksum updated from %x to %x\n", old_tcp_csum, tcp->check);
 }
 
 SEC("xdp_prog")
@@ -127,37 +126,34 @@ int xdp_redirect(struct xdp_md *ctx)
     __u8 src_mac[ETH_ALEN] = {0x98, 0xa9, 0x2d, 0x4f, 0x42, 0x8b};      // 源方向最近一跳的 MAC
     __u8 proxy_mac[ETH_ALEN] = {0x58, 0x11, 0x22, 0xc3, 0x23, 0xe6};    // 代理 MAC
     __u8 dst_mac[ETH_ALEN] = {0x98, 0xa9, 0x2d, 0x4f, 0x42, 0x8b};      // 目标方向最近一跳 MAC
-
-    // 从 172.20.62.99:3333 到 172.20.44.81:6666 的重定向
-    if (ip->saddr == bpf_htonl(SRC_IP) && tcp->source == bpf_htons(SRC_PORT) && ip->daddr == bpf_htonl(PROXY_IP) && tcp->dest == bpf_htons(PROXY_PORT))
+    //
+    // SRC --> PROXY --> DST
+    if (ip->saddr == bpf_htonl(SRC_IP) && ip->daddr == bpf_htonl(PROXY_IP) && tcp->dest == bpf_htons(TARGET_PORT))
     {
         __u32 old_tcp_csum = tcp->check;
         __u32 old_ip_csum = ip->check;
+        __u16 PROXY_PORT = bpf_ntohs(tcp->source);
+        __u16 DST_PORT = bpf_ntohs(tcp->dest);
         update_headers(data, data_end, eth, ip, tcp, proxy_mac, dst_mac, bpf_htonl(PROXY_IP), bpf_htonl(DST_IP), bpf_htons(PROXY_PORT), bpf_htons(DST_PORT));
         __u32 new_tcp_csum = tcp->check;
         __u32 new_ip_csum = ip->check;
-        bpf_printk("old ip csum %x new ip csum %x ", old_ip_csum, new_ip_csum);
-        bpf_printk("old tcp csum %x new tcp csum %x\n", old_tcp_csum, new_tcp_csum);
-        ret = bpf_redirect(ENO1_IFINDEX, 0);
-        // ret = XDP_PASS;
-        bpf_printk("Redirecting from src to dst: src port=%d, dst port=%d\n", bpf_ntohs(tcp->source), bpf_ntohs(tcp->dest));
-        return ret;    // 重定向
+        ret = bpf_redirect(DEVICE_IFINDEX, 0);
+        bpf_printk("redirecting from src to dst: src port %d dst port %d ret %d\n", bpf_ntohs(tcp->source), bpf_ntohs(tcp->dest), ret);
+        return ret;
     }
-    // 从 172.20.44.101:8811 到 172.20.44.81:6666 的重定向
-    else if (ip->saddr == bpf_htonl(DST_IP) && tcp->source == bpf_htons(DST_PORT) && ip->daddr == bpf_htonl(PROXY_IP) && tcp->dest == bpf_htons(PROXY_PORT))
+    // DST --> PROXY --> SRC
+    else if (ip->saddr == bpf_htonl(DST_IP) && tcp->source == bpf_htons(TARGET_PORT) && ip->daddr == bpf_htonl(PROXY_IP))
     {
         __u32 old_tcp_csum = tcp->check;
         __u32 old_ip_csum = ip->check;
+        __u16 PROXY_PORT = bpf_ntohs(tcp->source);
+        __u16 SRC_PORT = bpf_ntohs(tcp->dest);
         update_headers(data, data_end, eth, ip, tcp, proxy_mac, src_mac, bpf_htonl(PROXY_IP), bpf_htonl(SRC_IP), bpf_htons(PROXY_PORT), bpf_htons(SRC_PORT));
         __u32 new_tcp_csum = tcp->check;
         __u32 new_ip_csum = ip->check;
-        bpf_printk("old ip csum %x new ip csum %x ", old_ip_csum, new_ip_csum);
-        bpf_printk("old tcp csum %x new tcp csum %x\n", old_tcp_csum, new_tcp_csum);
-
-        ret = bpf_redirect(ENO1_IFINDEX, 0);
-        // ret = XDP_PASS;
-        bpf_printk("Redirecting from dst to src: src port=%d, dst port=%d\n", bpf_ntohs(tcp->source), bpf_ntohs(tcp->dest));
-        return ret;    // 重定向
+        ret = bpf_redirect(DEVICE_IFINDEX, 0);
+        bpf_printk("redirecting from dst to src: src port %d dst port %d ret %d\n", bpf_ntohs(tcp->source), bpf_ntohs(tcp->dest), ret);
+        return ret;
     }
 
     return XDP_PASS;    // 如果没有匹配，允许数据包通过
